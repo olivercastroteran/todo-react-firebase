59b3bba070be996df3eea5a89c8f204535f250c6
olivercastroteran <olivercastroteran@gmail.com>
2020-08-09 15:55:30 -0400

    updating todos

--- src/Todo.js
+++ src/Todo.js
@@ -3,9 +3,11 @@ import './Todo.css';
 import { ListItem, List, ListItemText, Modal } from '@material-ui/core';
 import db from './firebase';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 const Todo = (props) => {
   const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
 
   const style = {
     backgroundColor: 'white',
@@ -15,7 +17,18 @@ const Todo = (props) => {
     transform: 'translate(-50%, -50%)',
     width: '50%',
     padding: '1rem',
-    border: 'none',
+    border: '0',
+  };
+
+  const updateTodo = () => {
+    db.collection('todos').doc(props.todo.id).set(
+      {
+        todo: input,
+      },
+      { merge: true }
+    );
+
+    setOpen(false);
   };
 
   return (
@@ -23,14 +36,21 @@ const Todo = (props) => {
       <Modal open={open} onClose={(e) => setOpen(false)}>
         <div style={style}>
           <h1>Modal</h1>
-          <button onClick={(e) => setOpen(false)}>Close</button>
+          <input
+            placeholder={props.todo.todo}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={updateTodo}>Update Todo</button>
         </div>
       </Modal>
       <List className="todo-list">
         <ListItem>
           <ListItemText primary={props.todo.todo} secondary="Todo" />
         </ListItem>
-        <button onClick={(e) => setOpen(true)}>Edit</button>
+        <EditIcon className="edit-btn" onClick={(e) => setOpen(true)}>
+          Edit
+        </EditIcon>
         <DeleteIcon
           className="delete-btn"
           onClick={(e) => db.collection('todos').doc(props.todo.id).delete()}
