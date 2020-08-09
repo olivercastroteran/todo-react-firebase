0fa4b90812acabd5e8dd45bd95073b67bcdc9ad7
olivercastroteran <olivercastroteran@gmail.com>
2020-08-08 22:52:17 -0400

    modal styled

--- src/Todo.js
+++ src/Todo.js
@@ -7,10 +7,21 @@ import DeleteIcon from '@material-ui/icons/Delete';
 const Todo = (props) => {
   const [open, setOpen] = useState(false);
 
+  const style = {
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    padding: '1rem',
+    border: 'none',
+  };
+
   return (
     <>
       <Modal open={open} onClose={(e) => setOpen(false)}>
-        <div>
+        <div style={style}>
           <h1>Modal</h1>
           <button onClick={(e) => setOpen(false)}>Close</button>
         </div>
