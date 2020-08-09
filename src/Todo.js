c3701fa56540caee5ba79980e39ced0442200f6c
olivercastroteran <olivercastroteran@gmail.com>
2020-08-08 22:43:49 -0400

    material ui modal added

--- src/Todo.js
+++ src/Todo.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
-import { ListItem, List, ListItemText } from '@material-ui/core';
+import { ListItem, List, ListItemText, Modal } from '@material-ui/core';
 import db from './firebase';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const Todo = (props) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <List className="todo-list">
-      <ListItem>
-        <ListItemText primary={props.todo.todo} secondary="Todo" />
-      </ListItem>
-      <DeleteIcon
-        onClick={(e) => db.collection('todos').doc(props.todo.id).delete()}
-        color="secondary"
-        variant="outlined"
-      />
-    </List>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div>
+          <h1>Modal</h1>
+          <button onClick={(e) => setOpen(false)}>Close</button>
+        </div>
+      </Modal>
+      <List className="todo-list">
+        <ListItem>
+          <ListItemText primary={props.todo.todo} secondary="Todo" />
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>Edit</button>
+        <DeleteIcon
+          className="delete-btn"
+          onClick={(e) => db.collection('todos').doc(props.todo.id).delete()}
+          color="secondary"
+          variant="outlined"
+        />
+      </List>
+    </>
   );
 };
 
